9fb079a132310b8a21279a774a251e7ba0e82c08
K.Bailey <katebailey1230@gmail.com>
2017-02-22 12:19:31 -0500

    changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { create(:user) }
-    
-    
-    describe "attributes" do
-     it "should have name and email attributes" do
-       expect(user).to have_attributes(name: user.name, email: user.email)
-     end
-       
-       it "responds to role" do
-       expect(user).to respond_to(:role)
-     end
+    let(:user) { create(:user) }
+
+    describe 'attributes' do
+        it 'should have name and email attributes' do
+            expect(user).to have_attributes(name: user.name, email: user.email)
+        end
+
+        it 'responds to role' do
+            expect(user).to respond_to(:role)
+        end
     end
 end
