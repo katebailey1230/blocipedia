49825988b33d7a784659b82376e92bab50bfafee
K.Bailey <katebailey1230@gmail.com>
2017-02-24 14:40:31 -0500

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :wikis
+
+  get 'about' => 'welcome#about'
+  
   devise_for :users
   resources :users, only: [:show]
-
     
   get 'welcome/index'
 
