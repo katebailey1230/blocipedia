27c180d28c2ff0922c18eb4fca044c12e0c357f4
K.Bailey <katebailey1230@gmail.com>
2017-02-28 12:00:08 -0500

    checkpoint-6

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,18 @@ require 'random_data'
  5.times do
    user = User.create!(
    name:     RandomData.random_name,
-   email:    RandomData.random_name,
+   email:    Faker::Internet.email,
    password: RandomData.random_sentence
    )
- end
  users = User.all
+end
 
-puts "#{User.count} users created"
+25.times do
+  Wiki.create!(
+    title: Faker::Beer.unique.name,
+    body: Faker::Lorem.paragraphs(2),
+    private: false,
+    user_id: User.all.sample.id
+  )
+wiki = Wiki.all
+end
