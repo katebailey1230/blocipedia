f6a873fb22dd5ba3d03ce2a291ffbaaa3be64293
K.Bailey <katebailey1230@gmail.com>
2017-03-10 15:38:06 -0500

    Markdown

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,11 @@ end
   )
 wiki = Wiki.all
 end
+
+Wiki.create!(
+  title: 'My First Markdown Wiki',
+  body:
+  %Q{### There Is Something You Should Know!
+  This is my very first post using markdown!
+  user_id: User.sample.id
+)
